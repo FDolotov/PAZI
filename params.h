afd3249c68e775bf643d386d55c776b091f9e72b
SirJorah <59660051+FDolotov@users.noreply.github.com>
2020-12-28 03:02:10 +0300

    Update params.h

--- params.h
+++ params.h
@@ -4,16 +4,16 @@
 #include <gcrypt.h>
 #include <sys/types.h>
 
-gcry_mpi_t p;
-gcry_mpi_t a;
-gcry_mpi_t d;
-gcry_mpi_t q;
-gcry_mpi_t u;
-gcry_mpi_t v;
+extern gcry_mpi_t p;
+extern gcry_mpi_t a;
+extern gcry_mpi_t d;
+extern gcry_mpi_t q;
+extern gcry_mpi_t u;
+extern gcry_mpi_t v;
 
-gcry_mpi_t zero;
-gcry_mpi_t one;
-gcry_mpi_t two;
+extern gcry_mpi_t zero;
+extern gcry_mpi_t one;
+extern gcry_mpi_t two;
 
 extern void set_params();
 extern void release_memory();
